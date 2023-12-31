10d86de1a91aee839d62e654d90e072fc9c10da5
gabycode <86427446+gabycode@users.noreply.github.com>
2023-12-31 12:47:44 -0400

    portfolio development

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,10 @@
+import React, { Suspense, useEffect, useState } from "react";
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+// import CanvasLoader from "../Loader";
+
+const Computers = () => {
+  return <div>Computedrs</div>;
+};
+
+export default Computers;
