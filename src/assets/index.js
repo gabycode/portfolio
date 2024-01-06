ad9bd4c94335db6ac51a52a1c23eceeba946551e
gabycode <86427446+gabycode@users.noreply.github.com>
2024-01-06 00:30:16 -0400

    fix: import & export of imgs

--- src/assets/index.js
+++ src/assets/index.js
@@ -19,9 +19,9 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import watchlist from "./watchlist.jpg";
+import quizzical from "./quizzical.png";
+import tenzies from "./tenzies.png";
 
 export {
   logo,
@@ -42,7 +42,7 @@ export {
   shopify,
   starbucks,
   tesla,
-  carrent,
-  jobit,
-  tripguide,
+  watchlist,
+  quizzical,
+  tenzies,
 };
