621d56cb1ff912c298d7da49edba19c87de6f9b8
gabycode <86427446+gabycode@users.noreply.github.com>
2024-04-23 20:08:32 -0400

    add: project card and to div

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { github } from "../assets";
+import { github, eye } from "../assets";
 import { SectionWrapper } from "../hoc";
-import { projects } from "../constants";
+import { projects, wordpressProjects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({
@@ -55,6 +55,50 @@ const ProjectCard = ({
   );
 };
 
+const WordpressProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{ max: 45, scale: 1, speed: 450 }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+        <div className="relative w-full h-[230px] ">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="white-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+              <img src={eye} alt={eye} className="w-1/2 h-1/2 object-contain" />
+            </div>
+          </div>
+        </div>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+
 const Works = () => {
   return (
     <>
@@ -77,6 +121,16 @@ const Works = () => {
       <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} {...project} index={index} />
+        ))}
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7 justify-center">
+        {wordpressProjects.map((project, index) => (
+          <WordpressProjectCard
+            key={`project-${index}`}
+            {...project}
+            index={index}
+          />
         ))}
       </div>
     </>
