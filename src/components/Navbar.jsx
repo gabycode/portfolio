faa7670f77baf50ebe44e0961d650444d53a5865
gabycode <86427446+gabycode@users.noreply.github.com>
2024-04-24 18:04:55 -0400

    add: name logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
+import { logo, menu, close, nameLogo } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -15,15 +15,16 @@ const Navbar = () => {
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to="/"
-          className="flex items-center gap-2"
+          className="flex items-center"
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0);
           }}>
-          <img src={logo} alt={logo} className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Gabriella &nbsp; <span className="sm:block hidden">Rosario</span>
-          </p>
+          <img
+            src={nameLogo}
+            alt={nameLogo}
+            className="w-44 h-9 object-contain"
+          />
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
